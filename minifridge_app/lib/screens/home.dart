dbdc90c07db8f7f0c78c2aa8e93c1cc9e972c3c5
Erica Du <erica.c.du@gmail.com>
2020-05-26 17:32:33 -0400

    Add firebase auth and sign in anon.

---
+++ minifridge_app/lib/screens/home.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e); // TODO: show dialog with error
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Logout',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: _signOut,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ minifridge_app/lib/screens/login.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  Future<void> _signInAnonymously() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sign in')),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Sign in anonymously'),
+          onPressed: _signInAnonymously,
+        ),
+      ),
+    );
+  }
+}
