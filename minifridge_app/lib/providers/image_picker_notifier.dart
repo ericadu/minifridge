75d2e1bc9fc1f5fc03a3072c1806b5d13bc7b705
Erica Du <erica.c.du@gmail.com>
2020-08-21 13:34:03 -0700

    Add carousel slider.

--- minifridge_app/lib/providers/image_picker_notifier.dart
+++ minifridge_app/lib/providers/image_picker_notifier.dart
@@ -1,14 +1,19 @@
-
-
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:minifridge_app/services/firebase_analytics.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
 class ImagePickerNotifier extends ChangeNotifier {
   File _imageFile;
+  List<Asset> _images = List<Asset>();
+  List<ByteData> _bytes = List<ByteData>();
   StorageUploadTask _uploadTask;
+  ImageSource _source;
+  int _current = 0;
 
   final FirebaseStorage _storage = FirebaseStorage(storageBucket: 'gs://minifridge.appspot.com');
 
@@ -16,8 +21,12 @@ class ImagePickerNotifier extends ChangeNotifier {
 
   File get imageFile => _imageFile;
   StorageUploadTask get uploadTask => _uploadTask;
+  ImageSource get source => _source;
+  List<Asset> get images => _images;
+  List<ByteData> get bytes => _bytes;
+  int get current => _current;
 
-  bool hasImage() => imageFile != null;
+  bool hasImage() => imageFile != null || _images.length > 0;
   bool uploading() => _uploadTask != null;
 
   void startUpload(String userId) {
@@ -26,16 +35,53 @@ class ImagePickerNotifier extends ChangeNotifier {
     notifyListeners();
   }
 
+  void setCurrent(int index) {
+    _current = index;
+    notifyListeners();
+  }
+
+  Future<String> pickImages() async {
+    List<Asset> resultList;
+    String _error;
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images
+      );
+    } on Exception catch(e) {
+      _error = e.toString();
+      print(_error);
+    }
+
+    if (_error == null) {
+      _images = resultList;
+      _bytes = await Future.wait(resultList.map((asset) async => await asset.getByteData()));
+
+      notifyListeners();
+      return 'None';
+    } else {
+      return _error;
+    }
+  }
+
   Future pickImage(ImageSource source) async {
-    final pickedFile = await picker.getImage(source: source);
+    _source = source;
 
-    if (pickedFile != null) {
-      _imageFile = File(pickedFile.path);
+    if (source == ImageSource.camera) {
+      final pickedFile = await picker.getImage(source: source);
 
-      analytics.logEvent(name: 'add_item', parameters: {
-        'source': source == ImageSource.camera ? 'camera' : 'gallery'
-      });
-      
+      if (pickedFile != null) {
+        _imageFile = File(pickedFile.path);
+
+        analytics.logEvent(name: 'add_item', parameters: {
+          'source': source == ImageSource.camera ? 'camera' : 'gallery'
+        });
+        
+        notifyListeners();
+      }
+    } else {
+      String error = await pickImages();
       notifyListeners();
     }
   }
@@ -43,6 +89,9 @@ class ImagePickerNotifier extends ChangeNotifier {
   void clear() {
     _imageFile = null;
     _uploadTask = null;
+    _images = List<Asset>();
+    _bytes = List<ByteData>();
+    
     notifyListeners();
   }
 }
--- minifridge_app/lib/screens/add_item/image_upload.dart
+++ minifridge_app/lib/screens/add_item/image_upload.dart
@@ -1,9 +1,10 @@
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:minifridge_app/screens/add_item/upload_status.dart';
-import 'package:minifridge_app/screens/home.dart';
 import 'package:minifridge_app/providers/image_picker_notifier.dart';
 import 'package:minifridge_app/providers/auth_notifier.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:provider/provider.dart';
 
 class ImageUploadPage extends StatelessWidget {
@@ -14,10 +15,11 @@ class ImageUploadPage extends StatelessWidget {
 
     return Consumer2(
         builder: (BuildContext context, ImagePickerNotifier picker, AuthNotifier user, _) {
+          final double height = MediaQuery.of(context).size.height - 200;
 
           return Scaffold (
             appBar: AppBar(
-              title: Text('Selected Photo', style: TextStyle(color: Colors.white))
+              title: Text('${picker.images.length } photo(s) selected', style: TextStyle(color: Colors.black))
             ),
             bottomNavigationBar: BottomAppBar(
               child: Row(
@@ -36,13 +38,61 @@ class ImageUploadPage extends StatelessWidget {
                 ]
               )
             ),
-            body: Column(
+            body: Stack(
               children: [
-                UploadStatus(),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 40, horizontal: 40),
-                  child: Image.file(picker.imageFile),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: height,
+                    viewportFraction: 1.0,
+                    enlargeCenterPage: false,
+                    onPageChanged: (index, reason) {
+                      picker.setCurrent(index);
+                    }
+                  ),
+                  items: picker.bytes.map((item) {
+                    return Container(
+                      child: Center(
+                        child: Image.memory(item.buffer.asUint8List(), fit: BoxFit.cover, height: height)
+                      )
+                    );
+                  }).toList()
                 ),
+                UploadStatus(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 20),
+                            child: Text("Please ensure receipt fills most of image"),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: picker.images.map((img) {
+                              int index = picker.images.indexOf(img);
+                              return Container(
+                                width: 8.0,
+                                height: 8.0,
+                                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: picker.current == index
+                                    ? Color.fromRGBO(0, 0, 0, 0.9)
+                                    : Color.fromRGBO(0, 0, 0, 0.4),
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      )
+                    )
+                  ],
+                )
               ],
             )
           );
